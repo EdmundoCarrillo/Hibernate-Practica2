3bd256b891a33049a7f747753989f6933327c5fd
EdmundoCarrillo <edmundoc21@gmail.com>
2015-06-03 02:01:12 -0500

    Pruebas quitando Comentarios.

--- src/com/escom/practica2/dao/HibernateUtil.java
+++ src/com/escom/practica2/dao/HibernateUtil.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package com.escom.practica2.dao;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
 
-/**
- * Hibernate Utility class with a convenient method to get Session Factory
- * object.
- *
- * @author DARTH VADER
- */
+
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
